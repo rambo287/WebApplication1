34112eda63917a467ec324c12585839f60434b0b
黃弘源 <eric@kingwaytek.com>
2015-01-16 14:32:38 +0800

    第二次修改

--- WebApplication1/Controllers/HomeController.cs
+++ WebApplication1/Controllers/HomeController.cs
@@ -15,7 +15,7 @@ namespace WebApplication1.Controllers
 
         public ActionResult About()
         {
-            ViewBag.Message = "Your application description page.";
+            ViewBag.Message = "你的應用系統描述頁面.";
 
             return View();
         }
